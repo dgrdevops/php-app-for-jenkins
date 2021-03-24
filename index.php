284989eb33bd89c35f9e8ab113bdae7f34c08ffc
Denis Gromov <dgrdevops@icloud.com>
2021-03-24 18:35:15 +0200

    Version 3.1

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ else
             <li><a href="https://github.com/aws/aws-sdk-php">AWS SDK for PHP on GitHub</a></li>
         </ul>
         <h2> Some Picture</h2>
-		<img src="Me.png">
+		<img src="Me.png" style='height:90; width:90'>
     </section>
 
     <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
