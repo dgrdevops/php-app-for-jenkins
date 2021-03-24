766fe2769f6ed819e160e2858a802863380a215b
Denis Gromov <dgrdevops@icloud.com>
2021-03-24 18:27:21 +0200

    Version 3.0

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ else
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>PHP Application - AWS Elastic Beanstalk</title>
+    <title>PHP App - AWS Elastic Beanstalk - by Denis</title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Devops Engineer!</h1>
+        <h1><font color=blue>Congratulations <br><font color=blue>DevOps Engineer!!!</font></h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
@@ -45,6 +45,15 @@ else
             <li><a href="http://aws.amazon.com/php">PHP developer center</a></li>
             <li><a href="https://github.com/aws/aws-sdk-php">AWS SDK for PHP on GitHub</a></li>
         </ul>
+
+        <h2>Author:</h2>
+        <ul>
+            <li><a href="http://aws.amazon.com/sdkforphp">AWS SDK for PHP home</a></li>
+            <li><a href="http://aws.amazon.com/php">PHP developer center</a></li>
+            <li><a href="https://github.com/aws/aws-sdk-php">AWS SDK for PHP on GitHub</a></li>
+        </ul>
+        <h2> Some Picture</h2>
+		<img src="Me.png">
     </section>
 
     <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
